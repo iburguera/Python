0d25b7d1fa5c6556b2e62c5317ddde1795777aac
Iker Burguera <ikerburguera@gmail.com>
2016-04-18 23:36:20 +0200

    Create getPublicIP.py

---
+++ GetPublicIP/getPublicIP.py
@@ -0,0 +1,28 @@
+#--------------------------------------------------------------
+#		       SCRIPT PARA OBTENER IP PUBLICA con JSON
+#--------------------------------------------------------------
+# 
+# Author  : Iker Burguera
+# Date    : 18/04/2016
+# Version : 1.0
+#
+# Note    : Read README.MD For further instructions
+#--------------------------------------------------------------
+
+import urllib2
+import json
+
+def printIP(ip):
+	print "\n" 
+	print "-"*150
+	print "[+] %s" % ip
+	print "-"*150
+
+def getIP():
+	response = urllib2.urlopen('http://jsonip.com/')
+	data = json.load(response)                            # Response -> {"ip":"85.85.234.35","about":"/about","Pro!":"http://getjsonip.com"}
+	printIP(str(data['ip']))
+	#print json.dumps(data, indent=4, sort_keys=True)     # Esto es para imprimirlo bonito por consola
+
+#Main
+getIP()
