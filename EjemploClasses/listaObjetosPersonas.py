5fb6db4db7b1c4e3042f04ae9f18098c51f58cec
Iker Burguera <ikerburguera@gmail.com>
2016-08-23 22:52:43 +0200

    Update listaObjetosPersonas.py

--- EjemploClasses/listaObjetosPersonas.py
+++ EjemploClasses/listaObjetosPersonas.py
@@ -7,8 +7,6 @@ class Persona:
 		self.peso   		= peso
 		self.resistencia	= resistencia
 
-personas = []
-
 def imprimirListaValores(listaPersonas):
 	for i in range(0,len(listaPersonas)):
 		print "-"*80
@@ -24,12 +22,12 @@ def recopilarInformacionLista(numeroUsuarios):
 		edad   		= raw_input("Introduce el EDAD: ")
 		altura 		= raw_input("Introduce el ALTURA: ")
 		peso   		= raw_input("Introduce el PESO: ")
-		resistencia = raw_input("Introduce el RESISTENCIA: ")
+		resistencia 	= raw_input("Introduce el RESISTENCIA: ")
 
 		usuario = Persona(nombre,edad,altura,peso,resistencia)
 		personas.append(usuario)
 
-
+personas = []
 
 if __name__ == "__main__":
 	numeroUsuarios = raw_input("Introduce el numero de usuarios que quieres registrar: ")
